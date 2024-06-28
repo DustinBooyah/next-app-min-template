41b42535d371d012b35b7d22429314548bfd6019
Dustin <dustin@dustinaguilar.com>
2024-06-28 01:30:18 -0400

    layout for dashboard complete

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,22 +1,73 @@
-"use client"
+'use client';
 import { NavbarSimple } from "../ui/navbars/NavbarSimple";
-import { HeaderSimple } from "../ui/header/HeaderSimple";
+// import { HeaderSimple } from "../ui/header/HeaderSimple";
 // import { TableSort } from "./users/TableSort";
-import { TableReviews } from "./users/TableReviews";
+// import { TableReviews } from "./users/TableReviews";
+import { AppShell, Container, rem, useMantineTheme, Burger } from '@mantine/core';
+// import { MantineLogo } from '@mantinex/mantine-logo';
+import { ReactNode, useState } from 'react';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+import  AppMain  from '../components/AppMain/AppMain';
+import  HeaderNav  from '../components/HeaderNav/HeaderNav';
+import  FooterNav  from '../components/FooterNav/FooterNav';
 
- 
-export default function Layout({ children }: { children: React.ReactNode }) {
+type Props = {
+  children: ReactNode;
+};
+
+function Layout({ children }: Props) {
+  const theme = useMantineTheme();
+  const tablet_match = useMediaQuery('(max-width: 768px)');
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
-    
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <HeaderSimple/>
-      <NavbarSimple />
-      <div className="flex-none md:w-64">
-        
-       
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-      <TableReviews/>
-    </div>
+    <AppShell
+      layout="alt"
+      header={{ height: 60 }}
+      footer={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'md',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding={0}
+    >
+      <AppShell.Header
+        style={{
+          height: rem(60),
+          border: 'none',
+          boxShadow: tablet_match ? theme.shadows.md : theme.shadows.sm,
+        }}
+      >
+        <Container fluid py="sm" px="lg">
+          <HeaderNav
+            desktopOpened={desktopOpened}
+            mobileOpened={mobileOpened}
+            toggleDesktop={toggleDesktop}
+            toggleMobile={toggleMobile}
+          />
+        </Container>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        {/* <NavbarSimple onClose={toggleMobile} /> */}
+        <NavbarSimple  />
+        {/* <HeaderSimple/> */}
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <AppMain>{children}</AppMain>
+        {/* <TableReviews/> */}
+      </AppShell.Main>
+      <AppShell.Footer p="md">
+        <Container fluid px="lg">
+          <FooterNav />
+        </Container>
+      </AppShell.Footer>
+    </AppShell>
   );
 }
+
+export default Layout;
+
+
+    
